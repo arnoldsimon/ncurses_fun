8d08f8c7bf8d870eb33e4bc14d08fd2f9ab94225
arnoldsimon <arnoldsimon.m@gmail.com>
2023-09-06 15:57:20 +0530

    made snake move on its own

--- snake_game/snake_game.cpp
+++ snake_game/snake_game.cpp
@@ -1,4 +1,6 @@
 #include <vector>
+#include <chrono>
+#include <thread>
 #include <iostream>
 #include <curses.h>
 
@@ -8,8 +10,9 @@
 int main()
 {
 	//..init curses
-	initscr(); noecho(); cbreak();
+	initscr(); noecho(); cbreak(); 
 	curs_set(0);
+	nodelay(stdscr, TRUE);
 	keypad(stdscr, TRUE); //..terminal screen size
 
 	int scr_h, scr_w;
@@ -25,6 +28,7 @@ int main()
 	//snake and board
 	bool board[GAME_HEIGHT][GAME_WIDTH] = {0};
 	std::vector<std::vector<int>> snake; //the rep as a vector
+	int snake_dir;
 
 	//..misc
 	int count, move;
@@ -45,6 +49,7 @@ int main()
 
 	//..init vars
 	count = 0;
+	snake_dir = KEY_UP;
 	snake.push_back(std::vector<int>{GAME_HEIGHT/2, GAME_WIDTH/2}); //snake begin at center of screen
 	board[snake[0][0]][snake[0][1]] = true;							//marking snake head in board
 	//..main game loop
@@ -56,12 +61,9 @@ int main()
 		for (unsigned int i = 0; i < snake.size(); ++i)
 		{
 			if (i == 0)
-			{
 				mvwprintw(game_context, snake[i][0] + 1, snake[i][1] + 1, "%c", s_head);
-				continue;
-			}
-
-			mvwprintw(game_context, snake[i][0] + 1, snake[i][1] + 1, "%c", s_body);
+			else
+				mvwprintw(game_context, snake[i][0] + 1, snake[i][1] + 1, "%c", s_body);
 		}
 		box(game_context, 0, 0);
 		wrefresh(game_context);
@@ -70,10 +72,17 @@ int main()
 		//..READ INPUT AND MOVE SNAKE ACCORDINGLY
 		move = getch();
 		if (move == 'q')
+		{
 			break;
+		}
+		else if (move != ERR || move != snake_dir)
+		{
+			if (move == KEY_UP || move == KEY_DOWN || move == KEY_LEFT || move == KEY_RIGHT)
+				snake_dir = move;
+		}
 
 		//..check collision and move head acc to inp
-		if (move == KEY_UP)
+		if (snake_dir == KEY_UP)
 		{
 			if (snake[0][0] == 0 || board[snake[0][0] - 1][snake[0][1]] == true)
 				break;
@@ -81,7 +90,7 @@ int main()
 			board[snake[0][0] - 1][snake[0][1]] = true;
 			snake.insert(snake.begin(), std::vector<int>{snake[0][0] - 1, snake[0][1]});
 		}
-		else if (move == KEY_DOWN)
+		else if (snake_dir == KEY_DOWN)
 		{
 			if (snake[0][0] == GAME_HEIGHT-1 || board[snake[0][0] + 1][snake[0][1]] == true)
 				break;
@@ -89,7 +98,7 @@ int main()
 			board[snake[0][0] + 1][snake[0][1]] = true;
 			snake.insert(snake.begin(), std::vector<int>{snake[0][0] + 1, snake[0][1]});
 		}
-		else if (move == KEY_LEFT)
+		else if (snake_dir == KEY_LEFT)
 		{
 			if (snake[0][1] == 0 || board[snake[0][0]][snake[0][1] - 1] == true)
 				break;
@@ -97,7 +106,7 @@ int main()
 			board[snake[0][0]][snake[0][1] - 1] = true;
 			snake.insert(snake.begin(), std::vector<int>{snake[0][0], snake[0][1] - 1});
 		}
-		else if (move == KEY_RIGHT)
+		else if (snake_dir == KEY_RIGHT)
 		{
 			if (snake[0][1] == GAME_WIDTH-1 || board[snake[0][0]][snake[0][1] + 1] == true)
 				break;
@@ -105,9 +114,8 @@ int main()
 			board[snake[0][0]][snake[0][1] + 1] = true;
 			snake.insert(snake.begin(), std::vector<int>{snake[0][0], snake[0][1] + 1});
 		}
-		else
-			continue;
 
+		/*
 		//..snake grow (+1) after every 5 moves
 		++count;
 		if (count % 5 == 0)
@@ -117,6 +125,10 @@ int main()
 			board[snake.back()[0]][snake.back()[1]] = false;
 			snake.pop_back();
 		}
+		*/
+		std::this_thread::sleep_until(std::chrono::system_clock::now() + std::chrono::milliseconds(500)); 
+		board[snake.back()[0]][snake.back()[1]] = false;
+		snake.pop_back();
 	}
 
 	//..close curses
